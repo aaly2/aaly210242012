eb4f0198c4d63b2bbd7bca9eccacc48483589963
Ahmed <aaly2@jhu.edu>
2012-11-01 15:46:24 -0400

    Problem 1

--- problem1.R
+++ problem1.R
@@ -1,5 +1,13 @@
-function(filename='bmw1Series',
-         fcnname=dnorm(x,mean=0, sd=1,log=FALSE),
-         nrange=c(-5,5),nsample=10)
+problem1<-function(filename='bmw1Series',
+         fcnname=dnorm,
+         nrange=c(-5,5),nsample=100) {
+  outputtexfile = paste(filename, '.pdf', sep='')
+  pdf(outputtexfile)
+  xbeg = nrange[1]
+  xend = nrange[2]
+  xval = seq(from= xbeg, to= xend, length= nsample)
+  yval = fcnname(xval)
   
-  
+  plot(xval, yval,type="l")
+  dev.off()
+}
