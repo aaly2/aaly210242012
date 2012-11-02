650c8080bc37042f6b1fe98fb11237fb4e75c592
Ahmed <aaly2@jhu.edu>
2012-11-01 20:05:21 -0400

    Problem 1 documentation entered

--- problem2.R
+++ problem2.R
@@ -2,6 +2,12 @@ mydata<-read.csv('mydata.csv',header=TRUE,
                  sep=",",
                  "\"",dec=".",
                  fill=TRUE,comment.char="")
+attach(mydata)
+
+mytable<-tapply(Medal,INDEX=NOC,table)
 
 
-tapply(Medal,INDEX=NOC,table)
+save(mytable, list = character(),
+     file = "problem2.Rda",
+     ascii = FALSE, version = NULL, envir = parent.frame(),
+     eval.promises = TRUE, precheck = TRUE)
